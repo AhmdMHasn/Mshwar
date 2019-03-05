d833b6b4c4b692a61da9e35b63b1f501c78117cc
Ahmed M Hassan <ahmdmhasn@gmail.com>
2019-03-05 08:05:37 +0200

    Initial commit

---
+++ app/src/main/java/eg/com/iti/mshwar/utils/MyValues.java
@@ -0,0 +1,37 @@
+package eg.com.iti.mshwar.utils;
+
+public final class MyValues {
+
+    // Database username & password
+    public static final String DB_USERNAME = "";
+    public static final String DB_PASSWORD = "";
+
+    // Tables
+    public static final String TRIP_TABLE = "";
+    public static final String NOTES_TABLE = "";
+    public static final String USER_TABLE = "";
+
+    // Trip Table
+    public static final String COLUMN_TRIP_ID = "";
+    public static final String COLUMN_TRIP_NAME = "";
+    public static final String COLUMN_TRIP_START_POINT = "";
+    public static final String COLUMN_TRIP_END_POINT = "";
+    public static final String COLUMN_TRIP_REPETITION = "";
+    public static final String COLUMN_TRIP_STATUS = "";
+    public static final String COLUMN_TRIP_NOTES = "";
+    public static final String COLUMN_TRIP_TRIP_TYPE = "";
+    public static final String COLUMN_TRIP_USER_ID = "";
+
+    // Notes Table
+    public static final String COLUMN_NOTES_NOTE_ID = "";
+    public static final String COLUMN_NOTES_TRIP_ID = "";
+    public static final String COLUMN_NOTES_NOTE = "";
+
+    // User Table
+    public static final String COLUMN_USER_ID = "";
+    public static final String COLUMN_USER_NAME = "";
+    public static final String COLUMN_USER_EMAIL = "";
+    public static final String COLUMN_USER_PASSWORD = "";
+    public static final String COLUMN_USER_PICTURE = "";
+
+}
