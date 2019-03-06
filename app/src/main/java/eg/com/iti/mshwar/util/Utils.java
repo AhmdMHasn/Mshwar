659543789ee67e3b46c6624a5114c7327e33fe9a
ahmedsallamdd <ahmedsallamdd@gmail.com>
2019-03-06 11:41:25 +0200

    modified the Utils class

--- app/src/main/java/eg/com/iti/mshwar/util/Utils.java
+++ app/src/main/java/eg/com/iti/mshwar/util/Utils.java
@@ -2,36 +2,26 @@ package eg.com.iti.mshwar.util;
 
 public final class Utils {
 
-    // Database username & password
-    public static final String DB_USERNAME = "";
-    public static final String DB_PASSWORD = "";
-
     // Tables
-    public static final String TRIP_TABLE = "";
-    public static final String NOTES_TABLE = "";
-    public static final String USER_TABLE = "";
+    public static final String TRIP_TABLE = "trip";
+    public static final String USER_TABLE = "user";
 
     // Trip Table
-    public static final String COLUMN_TRIP_ID = "";
-    public static final String COLUMN_TRIP_NAME = "";
-    public static final String COLUMN_TRIP_START_POINT = "";
-    public static final String COLUMN_TRIP_END_POINT = "";
-    public static final String COLUMN_TRIP_REPETITION = "";
-    public static final String COLUMN_TRIP_STATUS = "";
-    public static final String COLUMN_TRIP_NOTES = "";
-    public static final String COLUMN_TRIP_TRIP_TYPE = "";
-    public static final String COLUMN_TRIP_USER_ID = "";
-
-    // Notes Table
-    public static final String COLUMN_NOTES_NOTE_ID = "";
-    public static final String COLUMN_NOTES_TRIP_ID = "";
-    public static final String COLUMN_NOTES_NOTE = "";
+    public static final String COLUMN_TRIP_ID = "id";
+    public static final String COLUMN_TRIP_NAME = "name";
+    public static final String COLUMN_TRIP_START_POINT = "start_point";
+    public static final String COLUMN_TRIP_END_POINT = "end_point";
+    public static final String COLUMN_TRIP_REPETITION = "repetition";
+    public static final String COLUMN_TRIP_STATUS = "status";
+    public static final String COLUMN_TRIP_NOTES = "notes";
+    public static final String COLUMN_TRIP_TRIP_TYPE = "type";
+    public static final String COLUMN_TRIP_USER_ID = "user_id";
 
     // User Table
-    public static final String COLUMN_USER_ID = "";
-    public static final String COLUMN_USER_NAME = "";
-    public static final String COLUMN_USER_EMAIL = "";
-    public static final String COLUMN_USER_PASSWORD = "";
-    public static final String COLUMN_USER_PICTURE = "";
+    public static final String COLUMN_USER_ID = "id";
+    public static final String COLUMN_USER_NAME = "name";
+    public static final String COLUMN_USER_EMAIL = "email";
+    public static final String COLUMN_USER_PASSWORD = "password";
+    public static final String COLUMN_USER_PICTURE = "profile_picture";
 
 }
