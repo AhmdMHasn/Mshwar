e341208347581b659dda10d21469c916745d9985
ismailelmogy <ismailelmogy0@gmail.com>
2019-03-18 20:44:29 +0200

    to handle onstart auto complete

--- app/src/main/java/eg/com/iti/mshwar/activity/TripActivity.java
+++ app/src/main/java/eg/com/iti/mshwar/activity/TripActivity.java
@@ -1,10 +1,15 @@
 package eg.com.iti.mshwar.activity;
 
+import android.app.AlarmManager;
 import android.app.DatePickerDialog;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -14,6 +19,13 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLng;
 
 import java.util.Calendar;
 
@@ -24,17 +36,17 @@ import eg.com.iti.mshwar.dialog.AddNoteDialog;
 import eg.com.iti.mshwar.service.MyReceiver;
 
 public class TripActivity extends AppCompatActivity implements AddNoteDialog.AddNoteDialogListener {
-    EditText editTxtTripName, editTxtStartPoint, editTxtEndPoint;
+    public static final String TAG ="Error";
+    EditText editTxtTripName;
     Spinner spinnerTripType, spinnerTripRepetition;
     TextView txtViewDate, txtViewTime;
     Button btnAddTrip;
     ImageView addNote;
-
     TripBean tripBean;
     TripDaoImpl tripImpl;
-
     Calendar calendar;
-    ImageView addNote;
+
+// To handle  conflict on github
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,15 +54,12 @@ public class TripActivity extends AppCompatActivity implements AddNoteDialog.Add
         setContentView(R.layout.activity_trip);
 
         editTxtTripName = findViewById(R.id.editTxt_trip_name);
-        editTxtStartPoint = findViewById(R.id.editTxt_start_point);
-        editTxtEndPoint = findViewById(R.id.editTxt_end_point);
         spinnerTripType = findViewById(R.id.spinner_type_trip);
         spinnerTripRepetition = findViewById(R.id.spinner_trip_repetition);
         txtViewDate = findViewById(R.id.txtView_date);
         txtViewTime = findViewById(R.id.txtView_time);
         btnAddTrip = findViewById(R.id.btn_add_trip);
         addNote = findViewById(R.id.add_note);
-
         tripBean = new TripBean();
         calendar = Calendar.getInstance();
 
@@ -59,76 +68,77 @@ public class TripActivity extends AppCompatActivity implements AddNoteDialog.Add
             public void onClick(View view) {
                 // code to start trip
                 tripBean.setName(editTxtTripName.getText().toString());
-                if(tripBean.getName()!= null && tripBean.getStartPoint() != null && tripBean.getEndPoint() != null) {
-                    /*
-                    && tripBean.getTime()!= null && tripBean.getDate() != null
-                     */
+
+
+                if(tripBean.getName()!= null && tripBean.getStartPoint() != null && tripBean.getEndPoint() != null
+                      && tripBean.getTime()!= null && tripBean.getDate() != null){
+
                     Toast.makeText(TripActivity.this, tripBean.getName()+"hello", Toast.LENGTH_LONG).show();
+                    setAlarm();
                     tripImpl = new TripDaoImpl();
                     tripImpl.addTrip(tripBean);
                 }
                 else Toast.makeText(TripActivity.this, R.string.add_trip_error_message, Toast.LENGTH_LONG).show();
 
-                tripImpl = new TripDaoImpl();
-                tripImpl.addTrip(tripBean);
-                setAlarm();
-            }
-        });
-
-
-        txtViewDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showDatePickerDialog();
-            }
-        });
-
-        txtViewTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showTimePickerDialog();
-            }
-        });
-
-        spinnerTripType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-               String tripType = adapterView.getItemAtPosition(i).toString();
-                tripBean.setType(tripType);
-
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-        spinnerTripRepetition.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                String tripRepetition = adapterView.getItemAtPosition(i).toString();
-                tripBean.setRepetition(tripRepetition);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-                     // No thing to do here
             }
 
         });
 
-        addNote.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AddNoteDialog dialog = new AddNoteDialog();
-                dialog.show(getSupportFragmentManager(), "dialog_add_note");
+
+                txtViewDate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        showDatePickerDialog();
+                    }
+                });
+
+                txtViewTime.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        showTimePickerDialog();
+                    }
+                });
+
+                spinnerTripType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        String tripType = adapterView.getItemAtPosition(i).toString();
+                        tripBean.setType(tripType);
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+
+                spinnerTripRepetition.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        String tripRepetition = adapterView.getItemAtPosition(i).toString();
+                        tripBean.setRepetition(tripRepetition);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+                        // No thing to do here
+                    }
+
+                });
+
+                addNote.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        AddNoteDialog dialog = new AddNoteDialog();
+                        dialog.show(getSupportFragmentManager(), "dialog_add_note");
+                    }
+                });
+
             }
-        });
 
-    }
 
-    @Override
+
     public void makeNote(String noteDescription) {
         tripBean.appendNotes(noteDescription);
     }
@@ -146,9 +156,10 @@ public class TripActivity extends AppCompatActivity implements AddNoteDialog.Add
                 calendar.set(Calendar.YEAR, selectedYear);
                 calendar.set(Calendar.MONTH, selectedMonth);
                 calendar.set(Calendar.DAY_OF_MONTH, selectedDay);
-
                 selectedMonth = selectedMonth + 1;
-                txtViewDate.setText(selectedDay + "/" + selectedMonth + "/" + selectedYear);
+                String dateStr = selectedDay + "/" + selectedMonth + "/" + selectedYear;
+                txtViewDate.setText(dateStr);
+                tripBean.setDate(dateStr);
             }
         }, mYear, mMonth, mDay);
         mDatePicker.setTitle("Select Date");
@@ -166,7 +177,9 @@ public class TripActivity extends AppCompatActivity implements AddNoteDialog.Add
             public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
                 calendar.set(Calendar.HOUR_OF_DAY, selectedHour);
                 calendar.set(Calendar.MINUTE, selectedMinute);
-                txtViewTime.setText( selectedHour + ":" + selectedMinute);
+                String timeStr = selectedHour + ":" + selectedMinute;
+                txtViewTime.setText( timeStr);
+                tripBean.setTime(timeStr);
             }
         }, hour, minute, false);
         mTimePicker.setTitle("Select Time");
@@ -192,4 +205,53 @@ public class TripActivity extends AppCompatActivity implements AddNoteDialog.Add
 
         tripBean.setAlarmId(String.valueOf(alarmID));
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        PlaceAutocompleteFragment placeAutocompleteFragmentStartPoint = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.editTxt_start_point);
+        if (placeAutocompleteFragmentStartPoint != null)
+            placeAutocompleteFragmentStartPoint.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+                @Override
+                public void onPlaceSelected(Place place) {
+                    // TODO: Get info about the selected place.
+                    Log.i(TAG, "Place: " + place.getName());
+                    tripBean.setStartPoint(place.getName().toString());
+                    LatLng myLatLong = place.getLatLng();
+                    tripBean.setStartPointLatitude(myLatLong.latitude);
+                    tripBean.setStartPointLongitude(myLatLong.longitude);
+
+                }
+
+                @Override
+                public void onError(com.google.android.gms.common.api.Status status) {
+                    // TODO: Handle the error.
+                    Log.i(TAG, "An error occurred: " + status);
+                }
+            });
+        else Toast.makeText(this, "Problem with loading page", Toast.LENGTH_LONG).show();
+
+
+        PlaceAutocompleteFragment placeAutoCompleteFragmentEndPoint = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.editTxt_end_point);
+        if (placeAutoCompleteFragmentEndPoint != null)
+            placeAutoCompleteFragmentEndPoint.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+                @Override
+                public void onPlaceSelected(Place place) {
+                    // TODO: Get info about the selected place.
+                    Log.i(TAG, "Place: " + place.getName());
+                    tripBean.setEndPoint(place.getName().toString());
+                    LatLng myLatLong = place.getLatLng();
+                    tripBean.setEndPointLatitude(myLatLong.latitude);
+                    tripBean.setEndPointLongitude(myLatLong.longitude);
+                }
+
+                @Override
+                public void onError(Status status) {
+                    // TODO: Handle the error.
+                    Log.i(TAG, "An error occurred: " + status);
+                }
+            });
+        else Toast.makeText(this, "Problem with loading page", Toast.LENGTH_LONG).show();
+    }
 }
+
