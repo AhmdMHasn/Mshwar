abbac2b2d57daaa8eef24669339d3f3d0c506e98
ismailelmogy <ismailelmogy0@gmail.com>
2019-03-22 21:24:07 +0200

    add notes head on the map

--- app/src/main/java/eg/com/iti/mshwar/activity/DialogActivity.java
+++ app/src/main/java/eg/com/iti/mshwar/activity/DialogActivity.java
@@ -4,15 +4,18 @@ import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-
+import android.widget.Toast;
+import eg.com.iti.mshwar.service.NoteHeadService;
 import eg.com.iti.mshwar.service.service;
 import eg.com.iti.mshwar.util.Utils;
 
 public class DialogActivity extends Activity {
     Intent ReceivedIntent;
+    private static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION = 2084;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,19 +34,23 @@ public class DialogActivity extends Activity {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
 
-                        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
-                                Uri.parse("http://maps.google.com/maps?saddr="
-                                        +ReceivedIntent.getDoubleExtra(Utils.COLUMN_TRIP_START_POINT_LATITUDE, 0)+ ","
-                                        +ReceivedIntent.getDoubleExtra(Utils.COLUMN_TRIP_START_POINT_LONGITUDE, 0)
-                                        +"(" + ReceivedIntent.getDoubleExtra(Utils.COLUMN_TRIP_START_POINT, 0) +")"
-                                        +"&daddr="
-                                        +ReceivedIntent.getDoubleExtra(Utils.COLUMN_TRIP_END_POINT_LATITUDE, 0)+ ","
-                                        +ReceivedIntent.getDoubleExtra(Utils.COLUMN_TRIP_END_POINT_LONGITUDE, 0)
-                                        +"(" + ReceivedIntent.getDoubleExtra(Utils.COLUMN_TRIP_END_POINT, 0) +")"
-                                ));
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                                !Settings.canDrawOverlays(getApplicationContext())) {
 
-                        startActivity(intent);
-                        DialogActivity.this.finish();
+                            //If the draw over permission is not available open the settings screen
+                            //to grant the permission.
+                            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                                    Uri.parse("package:" + getPackageName()));
+                            startActivityForResult(intent, CODE_DRAW_OVER_OTHER_APP_PERMISSION);
+                        } else {
+                            showNoteHead();
+                            openMap();
+
+                        }
+
+
+
+
                     }
                 })
                 .setNeutralButton("later", new DialogInterface.OnClickListener() {
@@ -86,4 +93,54 @@ public class DialogActivity extends Activity {
         AlertDialog dialog = builder.create();
         dialog.show();
     }
+
+    private void showNoteHead() {
+
+        startService(new Intent(DialogActivity.this, NoteHeadService.class));
+        finish();
+
+    }
+
+    public void openMap(){
+
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("http://maps.google.com/maps?saddr="
+                        +ReceivedIntent.getDoubleExtra(Utils.COLUMN_TRIP_START_POINT_LATITUDE, 0)+ ","
+                        +ReceivedIntent.getDoubleExtra(Utils.COLUMN_TRIP_START_POINT_LONGITUDE, 0)
+                        +"(" + ReceivedIntent.getDoubleExtra(Utils.COLUMN_TRIP_START_POINT, 0) +")"
+                        +"&daddr="
+                        +ReceivedIntent.getDoubleExtra(Utils.COLUMN_TRIP_END_POINT_LATITUDE, 0)+ ","
+                        +ReceivedIntent.getDoubleExtra(Utils.COLUMN_TRIP_END_POINT_LONGITUDE, 0)
+                        +"(" + ReceivedIntent.getDoubleExtra(Utils.COLUMN_TRIP_END_POINT, 0) +")"
+                ));
+
+        startActivity(intent);
+        DialogActivity.this.finish();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION) {
+
+            //Check if the permission is granted or not.
+            // Settings activity never returns proper value so instead check with following method
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (Settings.canDrawOverlays(this)) {
+                    showNoteHead();
+                    openMap();
+                } else { //Permission is not available
+                    Toast.makeText(this,
+                            "Draw over other app permission not available. Closing the application",
+                            Toast.LENGTH_SHORT).show();
+
+                    finish();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
 }
